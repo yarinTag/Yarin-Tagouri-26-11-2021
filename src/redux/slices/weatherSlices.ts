8e6ee443ba5bf6fb09c75e9e73c530c65b51c729
yarinTag <taguori5@gmail.com>
2021-11-29 22:05:19 +0200

    back to env

--- src/redux/slices/weatherSlices.ts
+++ src/redux/slices/weatherSlices.ts
@@ -4,7 +4,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { AutoWeatherProps } from "../../types";
 
 export const defaultParams = {
-  apikey: "AD9i2mfrpq0S2tAFi8o5JtLbWjp2q9bC",
+  apikey: `${process.env.REACT_APP_API_KEY}`,
   language: "en-us",
 };
 
